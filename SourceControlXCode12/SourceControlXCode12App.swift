3cde0cc13f4f6e66345928a85ecb9c49978bb233
Rob Yatteau <ryatteau@gmail.com>
2021-11-09 22:18:19 -0500

    Initial Commit

---
+++ SourceControlXCode12/SourceControlXCode12App.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlXCode12App.swift
+//  SourceControlXCode12
+//
+//  Created by Rob Yatteau on 11/9/21.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlXCode12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
